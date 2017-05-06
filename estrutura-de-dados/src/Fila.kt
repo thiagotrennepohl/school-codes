b2212dc7740d0a3645d25b4c1e4c02cdabd29229
thiago trenepohl <thiagotrenepohl@gmail.com>
2017-05-06 19:12:04 -0300

    First version of a Queue algorithm

---
+++ estrutura-de-dados/src/Fila.kt
@@ -0,0 +1,59 @@
+fun main (args: Array<String>){
+    val queue :Queue = Queue()
+    for (i in 0..2){
+        queue.offer(i)
+    }
+    var i: Int = 0
+    while(i <= 2){
+        println(queue.poll())
+        i++
+    }
+
+}
+
+class Queue (){
+    var tail = -1
+    var head = -1
+    var elements:Int = 0
+    val myQueue = Array<Int?>(3){ i -> null }
+
+
+    fun offer(k:Int) {
+            if (isFull() && myQueue[myQueue.lastIndex] != null) {
+                println(myQueue.lastIndex)
+                tail = 0
+                myQueue[tail] = k
+                elements++
+            } else {
+                tail++
+                myQueue[tail] = k
+                elements++
+                println("Index: $k")
+            }
+        }
+    fun showArray(){
+        for (i in myQueue.lastIndex downTo 0){
+            println("* ${myQueue[i]}")
+        }
+    }
+
+    fun poll(): Int?{
+        if(head == myQueue.lastIndex){
+            head = 0
+        }
+        var value: Int? = 0
+        head++
+        value = myQueue[head]
+        myQueue[head] = null
+        elements--
+        return value
+    }
+
+    fun isFull(): Boolean{
+        if(tail == myQueue.lastIndex) return true
+        return false
+    }
+
+
+
+}
