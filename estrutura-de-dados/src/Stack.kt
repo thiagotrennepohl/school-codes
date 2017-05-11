8353963651498e62d16f960033a18e7410550970
Thiago Trennepohl <thiago.tr@involves.com.br>
2017-05-11 13:02:11 -0300

    Add cheated Stack

---
+++ estrutura-de-dados/src/Stack.kt
@@ -0,0 +1,38 @@
+import java.util.ArrayList
+
+fun main (args: Array<String>){
+    val stack: Stack = Stack()
+    for (i in 0..9){
+        stack.push(i)
+    }
+    for(i in 0..9){
+        println(stack.pop())
+    }
+
+}
+
+
+class Stack(){
+    internal val myStack: ArrayList<Int> = ArrayList()
+    internal val top: Int = 0
+
+    fun pop(): Int{
+        var value = myStack.get(myStack.lastIndex)
+        myStack.removeAt(myStack.lastIndex)
+        return value
+    }
+
+    fun push(k:Int){
+        myStack.add(k)
+    }
+
+    fun isEmpty(): Boolean{
+        if (myStack.isEmpty()) return  true
+        return false
+    }
+
+    fun printStack(){
+        println(myStack.toString())
+    }
+
+}
